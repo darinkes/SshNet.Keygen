90ecdc2ed15b4972b075efc766f99f4850f492ef
Stefan Rinkes <stefan.rinkes@gmail.com>
2021-02-10 17:52:39 +0100

    Make AesEncryption Object reusable
    - resalt everytime KdfOptions are requested

--- SshNet.Keygen/SshKeyEncryption/SshKeyEncryptionAes256.cs
+++ SshNet.Keygen/SshKeyEncryption/SshKeyEncryptionAes256.cs
@@ -19,41 +19,26 @@ namespace SshNet.Keygen.SshKeyEncryption
     {
         public string CipherName => $"aes256-{_mode.ToString().ToLower()}";
         public string KdfName => "bcrypt";
-        public int BlockSize => _cipher.BlockSize;
+        public int BlockSize => 16;
 
         private const int SaltLen = 16;
         private const int Rounds = 16;
-        private readonly AesCipher _cipher;
-        private readonly byte[] _salt;
         private readonly Aes256Mode _mode;
+        private readonly byte[] _passPhraseBytes;
+        private readonly byte[] _salt;
 
         public SshKeyEncryptionAes256(string passphrase, Aes256Mode mode = Aes256Mode.Ctr)
         {
             _mode = mode;
-
-            var passPhraseBytes = Encoding.ASCII.GetBytes(passphrase);
-            var keyiv = new byte[48];
+            _passPhraseBytes = Encoding.ASCII.GetBytes(passphrase);
             _salt = new byte[SaltLen];
-            new Random().NextBytes(_salt);
-            new BCrypt().Pbkdf(passPhraseBytes, _salt, Rounds, keyiv);
-
-            var key = new byte[32];
-            Array.Copy(keyiv, 0, key, 0, 32);
-            var iv = new byte[16];
-            Array.Copy(keyiv, 32, iv, 0, 16);
-
-            _cipher = mode switch
-            {
-                Aes256Mode.Cbc => new AesCipher(key, new CbcCipherMode(iv), new PKCS7Padding()),
-                Aes256Mode.Ctr => new AesCipher(key, new CtrCipherMode(iv), new PKCS7Padding()),
-                _ => throw new CryptographicException("Unsupported AES Mode")
-            };
         }
 
         public byte[] KdfOptions()
         {
             using var stream = new MemoryStream();
             using var writer = new BinaryWriter(stream);
+            new Random().NextBytes(_salt);
             writer.EncodeString(_salt);
             writer.EncodeUInt(Rounds);
             return stream.ToArray();
@@ -61,7 +46,20 @@ namespace SshNet.Keygen.SshKeyEncryption
 
         public byte[] Encrypt(byte[] data)
         {
-            return _cipher.Encrypt(data);
+            var keyiv = new byte[48];
+            new BCrypt().Pbkdf(_passPhraseBytes, _salt, Rounds, keyiv);
+            var key = new byte[32];
+            var iv = new byte[16];
+            Array.Copy(keyiv, 0, key, 0, 32);
+            Array.Copy(keyiv, 32, iv, 0, 16);
+
+            var cipher = _mode switch
+            {
+                Aes256Mode.Cbc => new AesCipher(key, new CbcCipherMode(iv), new PKCS7Padding()),
+                Aes256Mode.Ctr => new AesCipher(key, new CtrCipherMode(iv), new PKCS7Padding()),
+                _ => throw new CryptographicException("Unsupported AES Mode")
+            };
+            return cipher.Encrypt(data);
         }
 
         public byte[] Encrypt(byte[] data, int offset, int length)
